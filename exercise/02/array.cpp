340a02623a80944a734e56ce17e51f31ebd0a1f3
Ivan Kvesić <ivan.kvesic@fer.hr>
2018-11-13 20:57:20 +0100

    Exercise 02 commit (almost all tests passed)

--- exercise/02/array.cpp
+++ exercise/02/array.cpp
@@ -1,1 +1,88 @@
 #include "array.h"
+#include <stdexcept>
+
+array::array() 
+{
+	this ->p = nullptr;
+	this ->n = 0;
+}
+
+array::array(const array& other) 
+{
+	alloc_array(other.size());
+	for (int i = 0; i < this->n; ++i) 
+	{
+		p[i] = other.at(i);
+	}
+}
+
+array::array(const int size, const double value):array()
+{
+	alloc_array(size);
+	for (int i = 0; i < size; i++)
+	{
+		p[i] = value;
+	}
+
+}
+
+array::~array() 
+{
+	if (this->p != nullptr) 
+	{
+		delete[] this->p; this->p = nullptr;
+	}
+	this->n = 0;
+}
+
+void array::alloc_array(const int size) 
+{
+	this->p = new double[size];
+	if (this->p == nullptr)
+	{
+		throw;
+	}
+
+	this->n = size;
+}
+
+int array::size() const
+{
+	return this->n;
+}
+
+double array::at(const int i) const
+{
+	if (! (i >= 0 && i < n)) 
+	{
+		throw std::out_of_range("Value of array out of range");
+	}
+	return p[i];
+}
+
+bool array::operator== (const array& other) const
+{
+	if (this->size() != other.size()) 
+	{
+		return 0;
+	}
+
+	for (int i = 0; i < size(); i++)
+	{
+		if (at(i) != other.at(i))
+			return 0;
+	}
+
+	return 1;
+
+}
+
+const array array::operator= (const array& other) 
+{
+	alloc_array(other.size());
+	for (int i = 0; i < this->n; i++)
+	{
+		p[i] = other.at(i);
+	}
+	return (*this);
+}
--- exercise/02/array.h
+++ exercise/02/array.h
@@ -3,5 +3,17 @@ class array
 {
 	double* p;
 	int n;
+
+private:
+	void alloc_array(const int size);
 public:
+	array();
+	array(const array& other);
+	array(const int size, const double value);
+	~array();
+	int size() const;
+	double at(const int i) const;
+
+	bool operator==(const array& other) const;
+	const array operator= (const array& other);
 };
